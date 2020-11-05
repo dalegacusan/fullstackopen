5e8afff92287e1bd101fb7759ff19b1437e57d56
Dale <dale@gacusan.com>
2020-11-05 19:47:46 +0800

    Finished 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import "./App.css";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 
 export default function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filterName, setFilterName] = useState('');
@@ -40,6 +36,23 @@ export default function App() {
 
     setFilterName(value);
   }
+
+  useEffect(() => {
+    console.log("Effect");
+
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        const notes = response.data;
+        console.log("Promise Fulfilled");
+        setPersons(notes);
+      });
+  }, []);
+
+  console.log("render", persons.length, "persons");
+
+
+
 
   return (
     <div className="App">
